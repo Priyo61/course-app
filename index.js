6e5f9c616dada67927587161ca7ca8a72baa119e
Priyobrata Sorokhaibam <priyobratasorokhaibam@Priyobratas-MacBook-Air.local>
2025-10-17 04:44:00 +0530

    add admin-routes

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "1234567890";
 const { z } = require("zod");
+const { adminRouter } = require("./routes/admin");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 app.use(express.json());
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
 
 app.listen(port, () => {
   console.log("connected to server");
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", (req, res) => {});
+
+adminRouter.post("/sigin", (req, res) => {});
+module.exports = {
+  adminRouter: adminRouter,
+};
